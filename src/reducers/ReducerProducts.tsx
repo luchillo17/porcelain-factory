81ecb00320362ec30f8e7ebc395239eb848d0ade
Luchillo <luchillo17@gmail.com>
2017-05-29 11:42:15 -0500

    Add ProductsReducer to reducers, fix reducer initial state

--- src/reducers/ReducerProducts.tsx
+++ src/reducers/ReducerProducts.tsx
@@ -3,8 +3,18 @@ import { omit } from 'lodash';
 
 import { SET_PRODUCT, DELETE_PRODUCT } from '../actions';
 
+const productsInitialState: Products = {
+  0: {
+    id: '0',
+    name: 'Baldosa',
+    type: 'Piso',
+    price: '60000',
+    considerations: '',
+  }
+};
+
 export const ProductsReducer: MyReducer<Products> =
-  (state, action) => {
+  (state = productsInitialState, action) => {
     switch (action.type) {
       case SET_PRODUCT:
         const product = action.payload;
