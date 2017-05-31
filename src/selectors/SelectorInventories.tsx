18df0b5624a50d936e1a0fa4f5f8cdd4dc9cab01
Luchillo <luchillo17@gmail.com>
2017-05-31 16:04:42 -0500

    Fix reserved quantity in inventories

--- src/selectors/SelectorInventories.tsx
+++ src/selectors/SelectorInventories.tsx
@@ -1,16 +1,26 @@
 import { createSelector } from 'reselect';
-import { filter, mapKeys, mapValues, lowerCase } from 'lodash';
+import { filter, mapKeys, mapValues, reduce, lowerCase } from 'lodash';
 
 const getProducts = (state: RXState) => state.products;
 const getInventories = (state: RXState) => state.inventories;
+const getOrderItems = (state: RXState) => state.orderItems;
 const getSearchTerm = (state: RXState) => lowerCase(state.searchTerm);
 
 export const filteredInventories = createSelector(
-  [getProducts, getInventories, getSearchTerm],
-  (products, inventories, searchTerm) => {
+  [getProducts, getInventories, getOrderItems, getSearchTerm],
+  (products, inventories, orderItems, searchTerm) => {
     inventories = mapValues(inventories, (inventory) => ({
       ...inventory,
       product: products[inventory.productId],
+      reserved: reduce(
+        orderItems,
+        (sum, orderItem) => {
+          const quantity = orderItem.productId === inventory.productId ?
+            orderItem.quantity : 0;
+          return sum + quantity;
+        },
+        0,
+      ),
     }));
 
     if (
